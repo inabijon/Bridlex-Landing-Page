85a1df175766e7ab8ec6701de0121df4f1330192
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-07-20 09:32:41 +0500

    Ready to deploy

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+    document.body.style.backgroundColor = '#52646e';
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
